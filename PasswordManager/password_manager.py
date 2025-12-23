fdb1799a88c5d9ba6f158a44f9bb57a80973c3b1
Gamidu Welikanna <gamiduwelikanna@Gamidus-MacBook-Air.local>
2025-12-23 22:50:27 +0530

    Password Manager - 2

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,11 +1,24 @@
+computer_password = "hello123"
+
 main_pwd = input("What is the main password? ")
 
+if main_pwd!= computer_password:
+    print("Incorrect password. Access denied.")
+    exit()
 
 def view():
-    pass
-
+    with open('passwords.txt','r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split('|')
+            
 def add():
-    pass
+    name = input("Enter the user name: ")
+    pwd = input("Enter the password: ")
+
+    with open('passwords.txt','a') as f:
+        f.write(name + '|' + pwd + '\n')
+        print("Password added successfully.")
 
 while True:
     mode = input("Would you like to view and existing password or add a new password? (view/add) or type 'q' to exit: ").lower()
@@ -20,4 +33,3 @@ while True:
     else:
         print("Invalid mode.")
         continue
-whdkqwhdk
