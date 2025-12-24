0020e425551dc01580cca8990d372fbf3f7a4325
Gamidu Welikanna <gamiduwelikanna@Gamidus-MacBook-Air.local>
2025-12-24 16:38:12 +0530

    password manager is done

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,26 +1,48 @@
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+import hashlib
+import getpass
 
-'''def generate_key():
-    key =Fernet.generate_key()
-    with open('key.key','wb') as key_file:
-        key_file.write(key)
+'''def write_salt():
+    import os
+    salt = os.urandom(16)
+    with open('salt.key', 'wb') as salt_file:
+        salt_file.write(salt)
 
-generate_key()'''
+write_salt()'''
 
-def load_key():
-    file = open('key.key','rb')
-    key = file.read()
-    file.close()
+def load_salt():
+    with open('salt.key', 'rb') as f:
+        return f.read()
+
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
     return key
 
-key = load_key()
-computer_password = "hello123"
+# Hashed master password (hash of "hello123")
+# To generate: hashlib.sha256("hello123".encode()).hexdigest()
+MASTER_PASSWORD_HASH = "27cc6994fc1c01ce6659c6bddca9b69c4c6a9418065e612c69d110b3f7b11f8a"
 
-main_pwd = input("What is the main password? ")
+main_pwd = getpass.getpass("What is the main password? ")
 
-if main_pwd!= computer_password:
+# Verify master password
+pwd_hash = hashlib.sha256(main_pwd.encode()).hexdigest()
+if pwd_hash != MASTER_PASSWORD_HASH:
     print("Incorrect password. Access denied.")
     exit()
+
+# Load salt and derive encryption key
+salt = load_salt()
+key = derive_key(main_pwd, salt)
+fer = Fernet(key)
 
 
 
@@ -29,14 +51,19 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split('|')
-            print(f"User: {user} | Password: {passw}")
+            try:
+                decrypted_pwd = fer.decrypt(passw.encode()).decode()
+                print(f"User: {user} | Password: {decrypted_pwd}")
+            except Exception as e:
+                print(f"User: {user} | Error decrypting password: {e}")
 
 def add():
     name = input("Enter the user name: ")
-    pwd = input("Enter the password: ")
+    pwd = getpass.getpass("Enter the password: ")
 
     with open('passwords.txt','a') as f:
-        f.write(name + '|' + pwd + '\n')
+        encrypted_pwd = fer.encrypt(pwd.encode()).decode()
+        f.write(name + '|' + encrypted_pwd + '\n')
         print("Password added successfully.")
 
 while True:
