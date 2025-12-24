4f78dfb8aaa44f804602fc3a22e91f84856fc502
Gamidu Welikanna <gamiduwelikanna@Gamidus-MacBook-Air.local>
2025-12-24 16:25:45 +0530

    password Manager -3

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,3 +1,19 @@
+from cryptography.fernet import Fernet
+
+'''def generate_key():
+    key =Fernet.generate_key()
+    with open('key.key','wb') as key_file:
+        key_file.write(key)
+
+generate_key()'''
+
+def load_key():
+    file = open('key.key','rb')
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
 computer_password = "hello123"
 
 main_pwd = input("What is the main password? ")
@@ -6,12 +22,15 @@ if main_pwd!= computer_password:
     print("Incorrect password. Access denied.")
     exit()
 
+
+
 def view():
     with open('passwords.txt','r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split('|')
-            
+            print(f"User: {user} | Password: {passw}")
+
 def add():
     name = input("Enter the user name: ")
     pwd = input("Enter the password: ")
