c6fb6cc660c137e04b37a60afd86d9ab5e068832
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-12-30 14:51:06 +0530

    Madlibs Generator - 2

--- MadlibsGenerator/madlibsGenerator.py
+++ MadlibsGenerator/madlibsGenerator.py
@@ -1,2 +1,17 @@
 with open("text.txt", "r") as f:
     text= f.read()
+
+words = []
+start_of_word = -1
+
+taregt_start = "<"
+target_end = ">"
+
+for i , char in enumerate(text):
+    if char == taregt_start:
+        start_word= i
+
+    if char == target_end and start_word!= -1:
+        word = text[start_word:i+1]
+        words.add(word)
+        start_word = -1
